125f41f65cc2ce977aa0710a3204bf66ba103b5a
KANGSEUNGHOON1 <xgde0324@gmail.com>
2025-02-20 13:01:45 +0900

    팝업창, 섹션 배경색 바꾸기 등

---
+++ site_ex/js/tab_visual.js
@@ -0,0 +1,38 @@
+window.addEventListener("load", function () {
+  const popupBtnLinks = document.querySelectorAll(".popup-btn .inner a");
+  const popupBtn = document.querySelector(".popup-btn");
+  const popupText = document.querySelector(".pop-text");
+
+  popupBtnLinks.forEach(function (item) {
+    item.addEventListener("click", function (e) {
+      e.preventDefault();
+      popupBtnLinks.forEach(function (link) {
+        link.classList.toggle("active");
+      });
+    });
+  });
+
+  popupBtn.addEventListener("click", function () {
+    popupText.style.display = popupText.style.display === "none" ? "block" : "none";
+  });
+
+  // 메뉴 탭 기능
+  const tabTitle = document.querySelectorAll(".tab-nav li a");
+  tabTitle.forEach(function (element) {
+    element.addEventListener("click", function (e) {
+      e.preventDefault();
+      tabTitle.forEach(function (item) {
+        item.classList.remove("active");
+      });
+      this.classList.add("active");
+
+      const tabItems = document.querySelectorAll(".tabitem");
+      tabItems.forEach(function (tab) {
+        tab.style.display = "none";
+      });
+
+      const target = element.getAttribute("href");
+      document.querySelector(target).style.display = "block";
+    });
+  });
+});
